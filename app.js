15fcfd9d90bcfa5a487f858374f782ecd1a9eb5f
pierce <pierceshih@gmail.com>
2019-05-06 19:49:19 +0800

    Add express-handlebars to create page template.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const exphbs = require('express-handlebars');
+
+// 透過 engine 語法定義所使用的樣版引擎（名稱、相關設定）
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+// 透過 set 語法設定顯示樣板 view engine 為 handlebars
+app.set('view engine', 'handlebars');
+
 
 app.get('/', (req, res) => {
-  res.send('HomePage');
+  res.render('index');
 })
 
 app.listen(port, () => {
